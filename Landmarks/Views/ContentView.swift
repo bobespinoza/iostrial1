656b973cdedf8d4d0bc8f4439cd366670981e67e
Bob Espinoza <108849390+bobespinoza@users.noreply.github.com>
2022-07-09 13:11:26 -0400

    Added ti iostrial1 repo

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Robert Espinoza on 7/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
